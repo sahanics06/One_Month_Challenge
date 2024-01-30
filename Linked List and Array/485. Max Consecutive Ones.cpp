40e96a5bbfc475f51e6ec138d2b46aefd75c9551
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2024-01-31 04:36:42 +0530

    Create 485. Max Consecutive Ones.cpp

---
+++ Linked List and Array/485. Max Consecutive Ones.cpp
@@ -0,0 +1,43 @@
+/*
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+
+  */
+
+// Solution
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int l=0, r=0, n=nums.size(), ans=0;
+        while(r<n)
+        {
+            if(nums[r]==1)
+            {
+                l=r;
+                while(r<n && nums[r]==1)
+                {
+                    ans=max(ans, r-l+1);
+                    r++;
+                }
+            }
+            r++;
+        }
+        return ans;
+    }
+};
+
