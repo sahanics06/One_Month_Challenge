81bdad10d2e29c966cc053873690ca4a53c7c0c4
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2024-02-09 01:30:41 +0530

    Create 46. Permutations.cpp

---
+++ Recursion and Backtracking/46. Permutations.cpp
@@ -0,0 +1,48 @@
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+ 
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+
+*/
+
+// Solution by Swapping
+
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(vector<int>&nums, int start)
+    {
+        if(start==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=start; i<nums.size(); i++)
+        {
+            swap(nums[i], nums[start]);
+            solve(nums, start+1);
+            swap(nums[i], nums[start]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums,0);
+        return ans;
+    }
+};
