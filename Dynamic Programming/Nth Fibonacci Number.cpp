084c11787fbee0347825c1feb5be191a915e76aa
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2024-03-03 01:41:21 +0530

    Create Nth Fibonacci Number.cpp

---
+++ Dynamic Programming/Nth Fibonacci Number.cpp
@@ -0,0 +1,104 @@
+/*
+Problem statement
+The n-th term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+
+    F(n) = F(n - 1) + F(n - 2), 
+    Where, F(1) = 1, F(2) = 1
+
+
+Provided 'n' you have to find out the n-th Fibonacci Number. Handle edges cases like when 'n' = 1 or 'n' = 2 by using conditionals like if else and return what's expected.
+
+"Indexing is start from 1"
+
+
+Example :
+Input: 6
+
+Output: 8
+
+Explanation: The number is ‘6’ so we have to find the “6th” Fibonacci number.
+So by using the given formula of the Fibonacci series, we get the series:    
+[ 1, 1, 2, 3, 5, 8, 13, 21]
+So the “6th” element is “8” hence we get the output.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+6
+
+
+Sample Output 1:
+8
+
+
+Explanation of sample input 1 :
+The number is ‘6’ so we have to find the “6th” Fibonacci number.
+So by using the given formula of the Fibonacci series, we get the series:    
+[ 1, 1, 2, 3, 5, 8, 13, 21]
+So the “6th” element is “8” hence we get the output.
+
+
+Expected time complexity :
+The expected time complexity is O(n).
+
+
+Constraints:
+1 <= 'n' <= 10000     
+Where ‘n’ represents the number for which we have to find its equivalent Fibonacci number.
+
+Time Limit: 1 second
+  */
+
+// Solution using DP tabular
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+        vector<int>dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++)
+        {
+                dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        cout<<dp[n];
+        
+}
+
+// Solution recursion and memoization
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int n, vector<int>&dp)
+{
+        if(n==1)
+                return 1;
+        if(n==0)
+                return 0;
+        if(dp[n]!=-1)
+                return dp[n];
+        return dp[n] = solve(n-1, dp)+solve(n-2, dp);
+}
+
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+        vector<int>dp(n+1, -1);
+        cout<<solve(n, dp);
+        
+}
